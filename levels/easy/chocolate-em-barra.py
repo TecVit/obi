2512588d7113bd05ff9d8876f9959334c6a4a24b
TecVit <vitorfreelancer021@gmail.com>
2025-06-01 22:16:28 -0300

    update: levels

--- levels/easy/chocolate-em-barra.py
+++ levels/easy/chocolate-em-barra.py
@@ -3,9 +3,13 @@ def main():
     x1, y1 = map(int, input().split())
     x2, y2 = map(int, input().split())
 
-    d = n // 2
+    lm = n // 2
+    ch = (x1 <= lm and x2 > lm) or (x2 <= lm and x1 > lm)
 
-    if abs(x1 - x2) >= d or abs(y1 - y2) >= d:
+    cm = n // 2
+    cv = (y1 <= cm and y2 > cm) or (y2 <= cm and y1 > cm)
+
+    if ch or cv:
         print("S")
     else:
         print("N")
